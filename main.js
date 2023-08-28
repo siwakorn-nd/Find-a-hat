c2a81797b7f4a5f66c19a8fb756b8d7c5534072e
krystalemblems <siwakorn.nd@gmail.com>
2023-08-28 15:50:49 +0700

    Provide comment in each fucntion

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class Field {
     get field() {
         return this._field;
     }
-
+    //Generate map function by using for loop.Pass rows and column by create global variable named rows and column
     createMap(rows, columns) {
         let map = [];
         
@@ -29,7 +29,7 @@ class Field {
         }
         return map;
     }
-
+    //Generate hat position by checking field columns and rows length then random rows and columns position then return index columns and rows as a coodinate x,y
     placeHat() {
         const rows = this._field.length;
         const columns = this._field[0].length;
@@ -41,13 +41,13 @@ class Field {
 
         this._field[this._hatRow][this._hatColumn] = hat;
     }
-
+    //Generate holes by calculate how much a holes will be shown on the map by random percentage of hole than create hole ramdomly similar the hat 
     placeHoles() {
         const rows = this._field.length;
         const columns = this._field[0].length;
 
         const totalCells = rows * columns;
-        const numHoles = Math.floor(totalCells * 0.1); // 15% of the cells as holes
+        const numHoles = Math.floor(totalCells * 0.1); // 10% of the cells as holes
 
         for (let i = 0; i < numHoles; i++) {
             let randomRow, randomColumn;
@@ -59,7 +59,7 @@ class Field {
             this._field[randomRow][randomColumn] = hole;
         }
     }
-
+    // Win checking by check the player has moved to coordinate of hat
     checkWin(row, column) {
         return row === this._hatRow && column === this._hatColumn;
     }
@@ -69,7 +69,7 @@ class Player {
         this._field = field;
         this._positionX = 0;
         this._positionY = 0;
-        this._field.field[this._positionY][this._positionX] = pathCharacter;
+        this._field.field[this._positionY][this._positionX] = pathCharacter; //set default player position by give a coordinate on field as [0,0] 
     }
 
     get positionX() {
@@ -81,9 +81,10 @@ class Player {
     }
 
     move(dx, dy) {
-        const newX = this._positionX + dx;
-        const newY = this._positionY + dy;
+        const newX = this._positionX + dx; //annouce new player position in x axis
+        const newY = this._positionY + dy;//annouce new player position in y axis
 
+        // Check if the move is valid and update the player's position
         if (
             newX >= 0 && newX < this._field.field[0].length &&
             newY >= 0 && newY < this._field.field.length &&
@@ -93,18 +94,20 @@ class Player {
             this._positionX = newX;
             this._positionY = newY;
             this._field.field[this._positionY][this._positionX] = pathCharacter;
-            
+            // Check if the player found the hat
             if (this._field.checkWin(newY, newX)) {
                 console.log("Congratulations! You found the hat. You win!");
                 this.playAgain();
             }
-        } else {
+        }   // Player hit a hole or a border, game over
+            else {
             console.log("Oops! You hit a border or a hole. Game over!");
             this.playAgain();
         }
     }
 
     playAgain() {
+        // Ask if the player wants to play again
         const input = create('Do you want to play again? (y/n): ');
         if (input.toLowerCase() === 'y') {
             const rows = this._field.field.length;
@@ -117,17 +120,19 @@ class Player {
             console.log(this._field.field.map(row => row.join('')).join('\n'));
             this.play();
         } else {
+            // Exit the game
             process.exit(0);
         }
     }
 
     play() {
+        // Main game loop to get player input and move
         while (true) {
             clear();
             console.log(this._field.field.map(row => row.join('')).join('\n'));
             const input = create('Enter your move (w/a/s/d): (w = up, s = down, a = left , d = right) ');
             
-            if (input === 'w') {
+            if (input === 'w') { 
                 this.move(0, -1);
             } else if (input === 'a') {
                 this.move(-1, 0);
